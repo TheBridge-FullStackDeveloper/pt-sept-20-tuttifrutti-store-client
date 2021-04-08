6340f7656ce700eaf97375d76c3a0b1bb705e705
elisasoto <elisasotoperez@gmail.com>
2021-04-09 00:11:36 +0200

    component modified using FontAwesome Icons | css styles added

--- src/components/Header/IconLink.jsx
+++ src/components/Header/IconLink.jsx
@@ -1,30 +1,30 @@
-import { IconButton } from "@material-ui/core";
+import "./iconLink.scss";
 
-import Tooltip from '@material-ui/core/Tooltip';
-import FavoriteIcon from "@material-ui/icons/Favorite";
-import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
-import FaceIcon from '@material-ui/icons/Face';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { faCartArrowDown } from "@fortawesome/free-solid-svg-icons";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
 
-export default function IconLink() {
+const icons = {
+  fav: faHeart,
+  cart: faCartArrowDown,
+  search: faSearch,
+  profile: faUserCircle,
+};
 
-
+// From Header component we should import this one and pass props: text, icon type and the onclick. Example as follows
+export default function IconLink({ text = "Profile", type = "profile", onClick }) {
   return (
-    <div className='button-container'>
-    <Tooltip title = "Favourite" arrow>
-        <IconButton color="primary" aria-label="add a favourite" onClick={() => { console.log('I will redirect to favorites page')}} >
-          <FavoriteIcon />
-        </IconButton>
-    </Tooltip>
-    <Tooltip title = "Add to Cart" arrow>
-        <IconButton color="primary" aria-label="add to cart" onClick={() => { console.log('I will redirect to the cart page')}}>
-          <AddShoppingCartIcon />
-        </IconButton>
-    </Tooltip>
-    <Tooltip title = "Login/Signup" arrow>
-        <IconButton color="primary" aria-label="sign in or register" onClick={() => { console.log('I will redirect to register page')}}>
-          <FaceIcon />
-        </IconButton>
-    </Tooltip>
-    </div>
+    <>
+      <div className="iconlink iconlink--hover">
+        <FontAwesomeIcon
+          icon={icons[type]}
+          className="iconlink__icon iconlink--hover"
+          onClick={() => { console.log(`I will redirect to ${text} page`)}}
+        />
+      </div>
+      <label class="iconlink__text">{text.toUpperCase()}</label>
+    </>
   );
 }
