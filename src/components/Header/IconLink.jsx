a21ba6713c93f1c243a6651cb0ccd8c0ac02b756
Ccastillo06 <ccastillon92@gmail.com>
2021-04-09 18:42:12 +0200

    Uninstall material UI and check icon in App

--- src/components/Header/IconLink.jsx
+++ src/components/Header/IconLink.jsx
@@ -1,10 +1,10 @@
-import "./iconLink.scss";
+import './iconLink.scss';
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHeart } from "@fortawesome/free-solid-svg-icons";
-import { faCartArrowDown } from "@fortawesome/free-solid-svg-icons";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
-import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { faCartArrowDown } from '@fortawesome/free-solid-svg-icons';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
 
 const icons = {
   fav: faHeart,
@@ -14,16 +14,15 @@ const icons = {
 };
 
 // From Header component we should import this one and pass props: text, icon type and the onclick. Example as follows
-export default function IconLink({ text = "", type = "", onClick }) {
+export default function IconLink({ text = '', type = '', onClick }) {
+ // TODO Change into a Link component after React Router is merged
   return (
-    <>
-      <div className="iconlink iconlink--hover" data-title={text}>
-        <FontAwesomeIcon
-          icon={icons[type]}
-          className="iconlink__icon iconlink--hover"
-          onClick={onClick}
-        />
-      </div>
-    </>
+    <div className="iconlink iconlink--hover" data-title={text}>
+      <FontAwesomeIcon
+        icon={icons[type]}
+        className="iconlink__icon iconlink--hover"
+        onClick={onClick}
+      />
+    </div>
   );
 }
