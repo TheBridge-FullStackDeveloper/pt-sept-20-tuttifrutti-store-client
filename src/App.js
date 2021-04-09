8de7f76b437f8d4cd5c897d093041db42781846f
ZinGit-Dev <zei.informa@gmail.com>
2021-04-09 18:33:52 +0200

    create navBar, Header, route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
-import './App.css';
+import "./App.css";
+import { Route, Redirect, Switch } from "react-router-dom";
+import Header from "./components/Header/Header";
+import NavBar from "./components/NavBar/NavBar.jsx";
 
 function App() {
-  return <div className="App">Tuttifrutti Store 🍍</div>;
+  return (
+    <div className="App">
+      <h3>TuttiFrutti store</h3>
+      <Header />
+      <NavBar />
+      <Switch>
+        <Route exact path="/home">
+          {/* home */}
+        </Route>
+        <Route exact path="/search">
+          {/* search component */}
+        </Route>
+        <Route exact path="/favs">
+          {/* go to favorites */}
+        </Route>
+        <Route exact path="/cart">
+          {/* go to shopping cart */}
+        </Route>
+        <Route exact path="/profile">
+          {/* go to profile */}
+        </Route>
+        <Route exact path="/category/:category">
+          {/* aquí va el componente category */}
+        </Route>
+        <Redirect to="/home" />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
