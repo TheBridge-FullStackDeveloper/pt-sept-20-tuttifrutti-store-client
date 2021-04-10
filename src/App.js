ac6e78b0298ffe0a5eedbb568cd4e8476b257f94
David Aguilar Alfayate <davidaguilaralfayate@MacBook-Pro-de-David.local>
2021-04-10 12:14:41 +0200

    product-card component finish

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import Hero from './components/Hero/Hero';
-
 import './App.css';
 import IconLink from './components/Header/IconLink';
+import ProductCard from './components/ProductCard/index';
+
+
+const cardprops =  { 
+  productName: "pineapple",
+  picture: "https://www.complementosdelcafe.com/1784-large_default/acme-taza-verde-flat-white-150ml.jpg",
+  price: 1,
+  description: "Y, viéndole don Quijote de aquella manera, con muestras de tanta tristeza, le dijo: Sábete, Sancho, que no es un hombre más que otro si no hace más que otro.",
+  brand: "tropical",
+}
+
 
 function App() {
   return (
@@ -9,6 +19,7 @@ function App() {
       Tuttifrutti Store 🍍
       <Hero />
       <IconLink text="Carrito de la compra" type="cart" onClick={() => {}} />
+      <ProductCard {...cardprops}/>
     </div>
   );
 }
