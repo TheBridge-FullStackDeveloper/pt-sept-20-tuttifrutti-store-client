b26729b2fee1fe317e459779f34379e5069fac71
David Aguilar Alfayate <davidaguilaralfayate@MacBook-Pro-de-David.local>
2021-04-10 16:36:04 +0200

    product-list-component

--- src/App.js
+++ src/App.js
@@ -3,18 +3,57 @@ import { Route, Switch } from 'react-router-dom';
 import Header from './components/Header/Header';
 import NavBar from './components/NavBar/NavBar.jsx';
 import Footer from './components/Footer/Footer';
-import ProductCard from './components/ProductCard/index';
+import ProductList from './components/ProductList/index';
 import Category from './pages/Category';
 
 import './App.css';
 
-const cardprops =  { 
-  productName: "pineapple",
-  picture: "https://www.complementosdelcafe.com/1784-large_default/acme-taza-verde-flat-white-150ml.jpg",
-  price: 1,
-  description: "Y, viéndole don Quijote de aquella manera, con muestras de tanta tristeza, le dijo: Sábete, Sancho, que no es un hombre más que otro si no hace más que otro.",
-  brand: "tropical",
-}
+const productListMock = [
+  {
+    category: 'bebidas',
+    stock: 20,
+    productRef: 5676777,
+    productName: 'verduras',
+    brand: 'alcoli',
+    price: 45,
+    picture:
+      'https://www.complementosdelcafe.com/1784-large_default/acme-taza-verde-flat-white-150ml.jpg',
+    description:
+      'Lucas ipsum dolor sit amet darth palpatine solo organa luke organa anakin solo padmé calrissian fresca',
+    weight: 7,
+    weightType: 'kg'
+  },
+
+  {
+    category: 'alimentos',
+    stock: 20,
+    productRef: 5676986,
+    productName: 'frutas',
+    brand: 'alcoli',
+    price: 45,
+    picture:
+      'https://st4.depositphotos.com/11095424/26573/i/1600/depositphotos_265730570-stock-photo-star-wars-droid-c3po-golden.jpg',
+    description:
+      'Y, viéndole don Quijote de aquella manera, con muestras de tanta tristeza, le dijo: Sábete, Sancho, que no es un hombre más que otro si no hace más que otro.',
+    weight: 6,
+    weightType: 'kg'
+  },
+
+  {
+    category: 'alimentos',
+    stock: 20,
+    productRef: 5676986,
+    productName: 'frutas',
+    brand: 'alcoli',
+    price: 45,
+    picture:
+      'https://st4.depositphotos.com/11095424/26573/i/1600/depositphotos_265730570-stock-photo-star-wars-droid-c3po-golden.jpg',
+    description:
+      'Y, viéndole don Quijote de aquella manera, con muestras de tanta tristeza, le dijo: Sábete, Sancho, que no es un hombre más que otro si no hace más que otro.',
+    weight: 6,
+    weightType: 'kg'
+  }
+];
 
 function App() {
   return (
@@ -25,7 +64,7 @@ function App() {
       <Switch>
         <Route exact path="/home">
           {/* home */}
-          <ProductCard {...cardprops} />
+          <ProductList title="product list" productListMock={productListMock} />
         </Route>
         <Route exact path="/search">
           {/* search component */}
