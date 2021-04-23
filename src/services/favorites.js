f8f2cfda36baca562154008877e918ba03424bd4
Fernando Carmona <fernando.carayu@gmail.com>
2021-04-23 16:56:34 +0200

    set favorites on useEffect and remove favorite

---
+++ src/services/favorites.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+const BASE_URL = 'http://localhost:4000/api';
+
+export const getFavoritesPopulated = async () => {
+  try {
+    const response = await axios.get(`${BASE_URL}/favorites/all`, {
+      withCredentials: true
+    });
+
+    return response.data.data;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
+
+export const deleteFavorite = async (productId) => {
+  try {
+    const response = await axios.put(
+      `${BASE_URL}/favorites/remove/${productId}`,
+      { withCredentials: true }
+    );
+
+    return response.data.data;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
