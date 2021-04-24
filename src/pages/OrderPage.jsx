2b043308d0b8e6fc81945391dc0183f85c990191
Fernando Carmona <fernando.carayu@gmail.com>
2021-04-24 11:24:18 +0200

    refactor protected pages to use WithAuthentication

--- src/pages/OrderPage.jsx
+++ src/pages/OrderPage.jsx
@@ -1,34 +1,26 @@
-import React, { useContext } from 'react';
+import React from 'react';
 
-import { UserContext } from '../context/User';
 import OrderCard from '../components/Ordercard';
-import LoginForm from '../components/LoginForm/LoginForm';
 
 import '../styles/OrderPage.scss';
 
 export default function OrderPage() {
-  const { user } = useContext(UserContext);
+  return (
+    <div>
+      <h3>MI PEDIDO</h3>
 
-  if (user) {
-    return (
-      <div>
-        <h3>MI PEDIDO</h3>
-
-        <div className="productList__box">
-          <OrderCard />
-        </div>
-
-        <a
-          className="buttoncart"
-          href="https://www.paypal.com/signin"
-          rel="noopener noreferrer"
-          target="__blank"
-        >
-          PAGAR
-        </a>
+      <div className="productList__box">
+        <OrderCard />
       </div>
-    );
-  } else {
-    return <LoginForm />;
-  }
+
+      <a
+        className="buttoncart"
+        href="https://www.paypal.com/signin"
+        rel="noopener noreferrer"
+        target="__blank"
+      >
+        PAGAR
+      </a>
+    </div>
+  );
 }
