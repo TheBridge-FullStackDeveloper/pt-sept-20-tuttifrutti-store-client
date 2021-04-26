9fa3df19490b6c8143690678dfc3115328325035
[tekilit] <67770867+tekilit@users.noreply.github.com>
2021-04-26 19:01:47 +0200

    pagination products

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,53 +1,6 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-
-import ProductList from '../components/ProductList/index';
-import { BASE_URL } from '../constants';
-
+import Pagination from '../components/Pagination';
 const INIT_URL = '/products?page=1';
 
 export default function Products() {
-  const [productsList, setProductList] = useState([]);
-  const [isFetching, setIsFetching] = useState(false);
-  const [nextPage, setNextPage] = useState(null);
-
-  useEffect(() => {
-    loadMoreProducts();
-  }, []);
-
-  function loadMoreProducts() {
-    setIsFetching(true);
-
-    axios
-      .get(BASE_URL + (nextPage || INIT_URL))
-      .then((res) => {
-        setProductList((prevProductList) => [
-          ...prevProductList,
-          ...res.data.data
-        ]);
-
-        setNextPage(res.data.nextPage);
-        setIsFetching(false);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
-
-  const cantLoadMore = productsList.length && !nextPage;
-
-  return (
-    <div>
-      <ProductList
-        title={'Products'}
-        productList={productsList}
-        isFetching={isFetching}
-      />
-
-      {isFetching && <p>Loading more products...</p>}
-      {!isFetching && !cantLoadMore && (
-        <button onClick={loadMoreProducts}>Load more</button>
-      )}
-    </div>
-  );
+  return <Pagination INIT_URL={INIT_URL} />;
 }
